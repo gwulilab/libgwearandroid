44f612a0fee719b0846d10e82dd28d9febc35a25
Nam Tran <tranngocnam97@gmail.com>
2019-04-19 02:03:08 -0400

    Working API

--- src/main/java/edu/gwu/seas/gwulilab/libgwearandroid/ApiService.java
+++ src/main/java/edu/gwu/seas/gwulilab/libgwearandroid/ApiService.java
@@ -1,19 +1,23 @@
 package edu.gwu.seas.gwulilab.libgwearandroid;
 
+import java.util.List;
+
 import io.reactivex.Single;
+import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.Header;
 import retrofit2.http.Headers;
 import retrofit2.http.POST;
 
 public interface ApiService {
 
-    @Headers("{Content-Type:application/json, x-api-key:TuAVwnAabd7MmZQ2fCVAX1KPCO1xXm7h6Of6cE9c}")
-    @GET("/")
-    Single<PatientData> getPatientData();
+    @Headers("Content-Type: application/x-www-form-urlencoded")
+    @GET("patient-data")
+    Call<List<PatientData>> getPatientData(@Header("x-api-key") String apiKey);
 
-    @Headers("{Content-Type:application/json, x-api-key:TuAVwnAabd7MmZQ2fCVAX1KPCO1xXm7h6Of6cE9c}")
-    @POST("/")
-    Single<PostResponse> sendPatientData(@Body PatientData patientData);
+    @Headers("x-api-key:TuAVwnAabd7MmZQ2fCVAX1KPCO1xXm7h6Of6cE9c")
+    @POST("patient-data")
+    Call<PostResponse> sendPatientData(@Body PatientData patientData);
 
 }
--- src/main/java/edu/gwu/seas/gwulilab/libgwearandroid/GWear.java
+++ src/main/java/edu/gwu/seas/gwulilab/libgwearandroid/GWear.java
@@ -1,8 +1,15 @@
 package edu.gwu.seas.gwulilab.libgwearandroid;
 
+import android.util.Log;
+
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import io.reactivex.Single;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -12,18 +19,48 @@ public class GWear {
     Retrofit retrofit;
     ApiService apiService;
 
-    public GWear() {
+    String apiKey;
+
+    public GWear(String baseUrl, String apiKey) {
         retrofit = new Retrofit.Builder()
-                .baseUrl("https://213xjpuh93.execute-api.us-east-1.amazonaws.com/beta/")
+                .baseUrl(baseUrl)
                 .addConverterFactory(GsonConverterFactory.create())
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .build();
 
         apiService = retrofit.create(ApiService.class);
+        this.apiKey = apiKey;
     }
 
-    public Single<PatientData> getPatientData() {
-        return apiService.getPatientData();
+    public List<PatientData> getPatientData() {
+        final List<PatientData> patientData = new ArrayList<>();
+
+        Call<List<PatientData>> call = apiService.getPatientData(apiKey);
+
+        call.enqueue(new Callback<List<PatientData>>() {
+            @Override
+            public void onResponse(Call<List<PatientData>> call, Response<List<PatientData>> response) {
+                if (response.isSuccessful()) {
+                    patientData.clear();
+                    patientData.addAll(response.body());
+
+                    Log.i("returned", patientData.toString());
+                } else {
+                    try {
+                        System.out.println(response.errorBody().string());
+                    } catch (IOException e) {
+                        System.out.println("Could not parse error body");
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<PatientData>> call, Throwable t) {
+                Log.e("db", t.toString());
+            }
+        });
+
+        return patientData;
     }
 
     public void sendPatientData(int id,
@@ -36,11 +73,33 @@ public class GWear {
         PatientData patientData = new PatientData();
 
         patientData.setId(id);
-        patientData.setMac(mac);
-        patientData.setMetric(metric);
-        patientData.setSensor(sensor);
         patientData.setTimestamp(timestamp);
+        patientData.setMac(mac);
+        patientData.setSensor(sensor);
         patientData.setValue(value);
+        patientData.setMetric(metric);
+
+        Call<PostResponse> postPatientData = apiService.sendPatientData(patientData);
+
+        postPatientData.enqueue(new Callback<PostResponse>() {
+            @Override
+            public void onResponse(Call<PostResponse> call, Response<PostResponse> response) {
+                if (response.isSuccessful()) {
+                    Log.i("returned", "Post successful!");
+                } else {
+                    try {
+                        System.out.println(response.errorBody().string());
+                    } catch (IOException e) {
+                        System.out.println("Could not parse error body");
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<PostResponse> call, Throwable t) {
+                Log.e("db", t.toString());
+            }
+        });
 
         apiService.sendPatientData(patientData);
     }
